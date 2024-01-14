068a27394a898fdce7df03a2497748f6e55a6c51
Brett <brett_neighbors@yahoo.com>
2024-01-14 10:35:19 -0700

    End of lesson code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React, {useState} from "react";
-import './App.css';
+import { useState } from "react";
+import SubscriberForm from "./SubscriberForm";
+import SubscriberList from "./SubscriberList";
 
 function App() {
+  const [subscribers, setSubscribers] = useState([]);
+
+  const createSubscriber = (newSubscriber) =>
+    setSubscribers((currentSubscribers) => [
+      newSubscriber,
+      ...currentSubscribers,
+    ]);
+
+  const deleteSubscriber = (indexToDelete) =>
+    setSubscribers((currentSubscribers) =>
+      currentSubscribers.filter((post, index) => index !== indexToDelete)
+    );
+
   return (
-    <SubscriberForm />
+    <>
+      <SubscriberForm createSubscriber={createSubscriber} />
+      <SubscriberList
+        subscribers={subscribers}
+        deleteSubscriber={deleteSubscriber}
+      />
+    </>
   );
 }
 
